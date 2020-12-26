782450107ed0910f944806d3ae5dac4f2a8dac71
Vadym Kinchur <vkincur@gmail.com>
2020-12-26 20:43:56 +0200

    Fix bugs with overflow in slider

--- app/static/js/before_upload.js
+++ app/static/js/before_upload.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
   const preview = document.querySelector('.carousel-inner');
   const imagesInput = document.querySelector('#images');
+  const UploadImg = document.querySelector('#upload-img');
+
+  UploadImg.addEventListener('click', () => {
+    imagesInput.click();
+  });
 
   imagesInput.addEventListener('change', function() {
     if (this.files) {
